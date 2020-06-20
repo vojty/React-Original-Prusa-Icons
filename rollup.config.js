b8ee94517fd4b8bce4748acc4af274c6d8bb3b9d
michalxfanta <michal.fanta@prusa3d.cz>
2020-06-20 13:49:27 +0200

    tsc cmd parameters

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import image from '@rollup/plugin-image'
+import pkg from './package.json'
 import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
@@ -10,13 +11,13 @@ export default {
     input: './src/lib/index.ts',
     output: [
         {
+            file: pkg.main,
             format: 'cjs',
-            dir: 'dist/',
             sourcemap: true
         },
         {
+            file: pkg.module,
             format: 'esm',
-            dir: 'dist/',
             sourcemap: true
         }
     ],
