1a900105576d5fb333a2e7a5a3a60c0dd2c7c8c4
michalxfanta <michal.fanta@prusa3d.cz>
2020-06-26 00:52:25 +0200

    lazy loaded overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import {
-  OriginalPrusaIconsFavicon,
-  PrusaConnectFavicon
-} from './lib'
+import React, { ComponentType, Suspense } from 'react'
 
+import { files as Favicons } from './lib/favicons.json'
+import Icon from './interfaces/Icon'
 import { Link } from 'react-router-dom'
-import React from 'react'
+import Spinner from './components/helpers/spinner'
 import styled from 'styled-components'
 
 const Div = styled.div`
@@ -14,17 +13,20 @@ const Div = styled.div`
 `
 
 function App() {
-
   return (
     <Div>
-      <h1>Icons</h1>
-      <Link to={`/icon/${PrusaConnectFavicon.defaultProps.componentName}/${PrusaConnectFavicon.defaultProps.folder}`}>
-        <PrusaConnectFavicon />
-      </Link>
-      
-      <Link to={`/icon/${OriginalPrusaIconsFavicon.defaultProps.componentName}/${OriginalPrusaIconsFavicon.defaultProps.folder}`}>
-        <OriginalPrusaIconsFavicon />
-      </Link>
+      <h1>Favicons</h1>
+      <Suspense fallback={<Spinner />}>
+        {Favicons.filter(favicon => !!favicon.file).map((favicon, i) => {
+
+          const FaviconCompoment = React.lazy<ComponentType<Icon>>(() => import(`./lib/favicons/${favicon.file.replace('.tsx', '')}`))
+          return (
+            <Link key={i} to={`/icon/${favicon.file.replace('.tsx', '')}/favicons`}>
+              <FaviconCompoment />
+            </Link>
+          )
+        })}
+      </Suspense>
     </Div>
   )
 }
